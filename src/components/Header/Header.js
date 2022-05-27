e5834c590d724f38d9a295def97913ab5889bce3
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-05-27 18:09:29 +0100

    refactor function to add type to make it reusable and simplify code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,23 +9,14 @@ export const Header = ({ title }) => {
   const location = useLocation();
   const { selectedTerminals, dispatch } = useContext(TerminalsContext);
 
-  const addToScanedTerminals = () => {
+  const addTypeToTerminal = (type) => {
     selectedTerminals.forEach(terminal => {
-      terminal.actionType = 'scan';
+      terminal.actionType = type;
       terminal.date = new Date().toLocaleString();
-      dispatch({ type: 'SCAN_TERMINAL', payload: terminal });
-      dispatch({ type: 'CLEAR_SELECTED_TERMINALS'});
-    });
-  };
-
-  const addToTerminatedTerminals = () => {
-    selectedTerminals.forEach(terminal => {
-      terminal.actionType = 'terminate';
-      terminal.date = new Date().toLocaleString();
-      dispatch({ type: 'TERMINATE_TERMINAL', payload: terminal });
+      dispatch({ type: `${type}_TERMINAL`, payload: terminal });
       dispatch({ type: 'CLEAR_SELECTED_TERMINALS' });
     });
-  };
+  }
 
   return (
     <>
@@ -44,7 +35,7 @@ export const Header = ({ title }) => {
             justifyContent="center">
             <Button
               variant="outlined"
-              onClick={addToScanedTerminals}
+              onClick={() => addTypeToTerminal('SCAN')}
             >Scan</Button>
           </Grid>
           <Grid item xs={4}
@@ -52,7 +43,7 @@ export const Header = ({ title }) => {
             justifyContent="center">
             <Button
               variant="outlined"
-              onClick={addToTerminatedTerminals}
+              onClick={() => addTypeToTerminal('TERMINATE')}
             >Terminate</Button>
           </Grid>
 
