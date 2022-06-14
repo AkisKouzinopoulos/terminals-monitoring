2a3319ae48f85b5cfa1508c2c2169f393a02a0fd
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-06-14 18:49:53 +0100

    add prettier and eslint files

--- src/context/Terminals/TerminalsReducer.js
+++ src/context/Terminals/TerminalsReducer.js
@@ -1,48 +1,50 @@
-const githubReducer = (state, action) => {
+const terminalsReducer = (state, action) => {
   switch (action.type) {
     case 'SELECT_TERMINAL':
       return {
         ...state,
         selectedTerminals: [action.payload, ...state.selectedTerminals],
-      }
+      };
     case 'DESELECT_TERMINAL':
       return {
         ...state,
-        selectedTerminals: [...state.selectedTerminals.filter(item => item.id !== action.payload.id)],
-      }
+        selectedTerminals: [
+          ...state.selectedTerminals.filter(item => item.id !== action.payload.id),
+        ],
+      };
     case 'SCAN_TERMINAL':
       return {
         ...state,
         scanedTerminals: [action.payload, ...state.scanedTerminals],
         // isLoading: false,
-      }
+      };
     case 'TERMINATE_TERMINAL':
       return {
         ...state,
         terminatedTerminals: [action.payload, ...state.terminatedTerminals],
         // isLoading: false,
-      }
+      };
     case 'UPDATE_LOGS':
       return {
         ...state,
         logs: [action.payload, ...state.logs],
         // isLoading: false,
-      }
+      };
     case 'CLEAR_SELECTED_TERMINALS':
       return {
         ...state,
         selectedTerminals: [],
-      }
+      };
     case 'CLEAR_STATE_TERMINALS':
       return {
         ...state,
         selectedTerminals: [],
         scanedTerminals: [],
         terminatedTerminals: [],
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default githubReducer;
+export default terminalsReducer;
